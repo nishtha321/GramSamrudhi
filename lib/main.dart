e24654a3568fa0e9ce8b030b954d210363f7b985
nishtha321 <https://github.com/nishtha321>
2021-08-10 20:19:54 +0530

    Ui of first two page

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:login_pages/phone_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.rubikTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: PhoneScreen(),
+    );
+  }
+}
